0e69f9ba703ff59d8d8942f663ea989f6e153f4f
Pedro Silva <pedro87silva@gmail.com>
2015-04-29 02:47:50 +0100

    Criação da JFrame do Jogo e respectivas definições, Iniciação do Processo de Render, fase de Testes

--- src/Graphics/Screen.java
+++ src/Graphics/Screen.java
@@ -2,4 +2,33 @@ package Graphics;
 
 
 public class Screen {
+
+
+    private int width;
+    private int height;
+    private int[] pixels;
+
+    public Screen(int width, int height){
+        this.width = width;
+        this.height = height;
+        this.pixels = new int[this.width * this.height];
+    }
+
+    public void clear(){
+        for(int i = 0; i < (this.width * this.height); i++){
+            this.pixels[i] = 0;
+        }
+    }
+
+    public int[] getPixels(){
+        return this.pixels;
+    }
+
+    public void render(){
+        for(int y = 0; y < height; y++){
+            for(int x = 0; x < width; x++){
+                pixels[x + y * width] = 0xff00ff;
+            }
+        }
+    }
 }
--- src/Main/Game.java
+++ src/Main/Game.java
@@ -1,9 +1,12 @@
 package Main;
 
-import javafx.stage.Screen;
-
+import Graphics.Screen;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBuffer;
+import java.awt.image.DataBufferInt;
 
 /**
  * Created by pedroreissilva on 27/04/15.
@@ -12,23 +15,86 @@ public class Game extends Canvas implements Runnable{
 
     private boolean isRunning;
     private Screen screen;
+    private JFrame gameWindow;
+    private static int WIDTH = 800;
+    private static int HEIGHT = 600;
+
+    /* We will create a image buffer to load the tiles, this buffer is the size of screen and the window,
+    the final result will be an array with colors, or tiles;
+    */
+    private BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+    private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
 
     public Game(){
+        //instantiate the window frame and the screen
+        gameWindow = new JFrame();
+        screen = new Screen(WIDTH, HEIGHT);
+
+        gameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gameWindow.setLocationRelativeTo(null);
+        gameWindow.setSize(new Dimension(WIDTH, HEIGHT));
+        gameWindow.setVisible(true);
 
     }
 
     public void run(){
-        try{
-            this.gameLoop();
-        }catch(){
+        this.requestFocus();
+        /*
+        We will save de last time because when we start the method run, he takes a few nanoseconds to enter in while cicle,
+        when java enter in cicle we will try to know the diference between that times
 
+        Dont forget:
+
+        1 sec = 1000 milliseconds;
+        1000 milliseconds  = 1000000000 nanoseconds;
+
+        1000 milliseconds / 60 (fps) = 16.666666667 milliseconds;
+
+        16.666666667 milliseconds = 16666666.667 nanoseconds;
+
+        1000000000.0 / 60.0 = 16666666.667 nanoseconds;
+
+         */
+        long lastNanoTime = System.nanoTime();
+        long timer = System.currentTimeMillis();
+        final double nanoSeconds = 1000000000.0 / 60.0;
+        double delta = 0;
+        int updates = 0;
+        int frames = 0;
+        while(isRunning){
+            long nowNanoTime = System.nanoTime();
+            delta += (nowNanoTime - lastNanoTime) / nanoSeconds;
+            lastNanoTime = nowNanoTime;
+
+            /*
+             example of delta 0.27979285999997355, when delta pass or equal 1 that means is time to update,
+             the game have to update 60 times per second FPS
+             */
+            while (delta >= 1){
+                this.update();
+                updates++;
+                delta--;
+            }
+
+            this.render();
+            frames++;
+
+            if(System.currentTimeMillis() - timer > 1000){ //passed one second
+                //System.out.println(System.currentTimeMillis());
+                timer += 1000;
+                gameWindow.setTitle("Updates: " + updates + " --- Frames: " + frames);
+                updates = 0;
+                frames = 0;
+            }
         }
+
     }
 
 
     public synchronized void start(){
         isRunning = true;
-        this.start();
+        Thread thread = new Thread(this);
+        thread.start();
 
     }
 
@@ -36,17 +102,55 @@ public class Game extends Canvas implements Runnable{
         isRunning = false;
     }
 
-
-    public void gameLoop(){
-
-    }
-
+    /**
+     * The method render will render the screen tiles and present them to the gamer
+     */
     public void render(){
 
+        /*Triple buffering:
+
+        [To          ***********         ***********        ***********
+            display] *         * --------+---------+------> *         *
+               <---- * Front B *   Show  * Mid. B. *        * Back B. * <---- Rendering
+                     *         * <------ *         * <----- *         *
+                     ***********         ***********        ***********
+
+        */
+        BufferStrategy bufferStrategy = gameWindow.getBufferStrategy();
+        if(bufferStrategy == null){
+            gameWindow.createBufferStrategy(3);
+            return;
+        }
+
+        screen.clear();
+
+        screen.render();
+
+        for(int i = 0; i < (getWidth() * getHeight()); i++){
+            this.pixels[i] = screen.getPixels()[i];
+        }
+
+
+        Graphics graphics = bufferStrategy.getDrawGraphics(); // connect the graphics to the buffer created by me
+        graphics.drawImage(image, 0, 0, null);
+
+        /*
+        Free resources allocated by Graphics, normally the Garbage collector clean
+        this memory automatically, but for better performance we will free manually
+        */
+        graphics.dispose();
+        bufferStrategy.show();
     }
 
     public void update(){
+    }
 
+    public int getHeight(){
+        return this.HEIGHT;
+    }
+
+    public int getWidth(){
+        return this.WIDTH;
     }
 
 
